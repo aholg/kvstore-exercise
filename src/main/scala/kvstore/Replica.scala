6dc48dd86de95f044a31f49fa76809a89a6184be
Anton Holgersson <karl-anton.holgersson@springer.com>
2020-05-29 14:19:25 +0200

    Add persistance for secondary

--- src/main/scala/kvstore/Replica.scala
+++ src/main/scala/kvstore/Replica.scala
@@ -43,9 +43,21 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   var secondaries = Map.empty[ActorRef, ActorRef]
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
+  var pendingPersistances = Map.empty[Long, (ActorRef, Persist)]
 
   var seqNumber = 0L
+  private val persistence: ActorRef = context.actorOf(persistenceProps)
 
+  context.system.scheduler.scheduleWithFixedDelay(100.millis, 100.millis) {
+    new Runnable {
+      override def run(): Unit = {
+        pendingPersistances.foreach { entry =>
+          val persistanceEntry = entry._2._2
+          persistence ! persistanceEntry
+        }
+      }
+    }
+  }
 
   def receive = {
     case JoinedPrimary => context.become(leader)
@@ -73,11 +85,20 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
         case Some(value) => kv = kv.updated(key, value)
         case None => kv = kv.removed(key)
       }
-      seqNumber += 1L
-      sender ! SnapshotAck(key, seq)
+      val persist = Persist(key, maybeValue, seq)
+      pendingPersistances += (seq -> (sender, persist))
+
+      persistence ! persist
     case Snapshot(key, _, seq) if seq < seqNumber =>
       sender ! SnapshotAck(key, seq)
-
+    case Persisted(key, id) =>
+      pendingPersistances = pendingPersistances.get(id) match {
+        case Some(persistanceEntry) =>
+          seqNumber += 1L
+          persistanceEntry._1 ! SnapshotAck(key, id)
+          pendingPersistances.removed(id)
+        case _ => pendingPersistances
+      }
   }
 
   arbiter ! Join
